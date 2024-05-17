ee012c68f7aa96d0c8bc5baec1e1bad4c696d2c7
luys <sanchezluys@gmail.com>
2024-05-17 14:19:11 -0500

    hola mundo con spring

---
+++ screenmatch/src/main/java/com/aluracursos/sanchezluys/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,18 @@
+package com.aluracursos.sanchezluys.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("hola mundo desde spring");
+	}
+}
