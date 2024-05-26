381bca5f11364bd8cebf9271db344b08d88f88ab
luys <sanchezluys@gmail.com>
2024-05-26 18:58:52 -0500

    Encabezado listo, con spring listo

--- screenmatch/src/main/java/com/aluracursos/sanchezluys/screenmatch/RetoLibros.java
+++ screenmatch/src/main/java/com/aluracursos/sanchezluys/screenmatch/RetoLibros.java
@@ -1,5 +1,6 @@
 package com.aluracursos.sanchezluys.screenmatch;
 
+import com.aluracursos.sanchezluys.screenmatch.principal.Menus;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,8 @@ public class RetoLibros implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Iniciando proyecto Libros - Reto del curso");
+		Menus menu = new Menus();
+		menu.Encabezado();
+
 	}
 }
---
+++ screenmatch/src/main/java/com/aluracursos/sanchezluys/screenmatch/principal/Menus.java
@@ -0,0 +1,20 @@
+package com.aluracursos.sanchezluys.screenmatch.principal;
+
+public class Menus {
+   public void Encabezado(){
+        // emoticones: ⚠️🔥📝✅❎📂⚙️🏷️📦📆🗓️🕑🏁➡️⬆️🎁🛒🛍️✏️🖍️✂️🔒🔓💎😀😃
+        //**
+        System.out.println("╔════════════════════════════════════════════════════════╗");
+        System.out.println("║ ALURA LATAM    MAYO 2024  BY: SANCHEZLUYS              ║");
+        System.out.println("║ RETO: LIBROS DEL CURSO DE JAVA LAMBDAS/SPRING          ║");
+        System.out.println("║ Profesor(es): Genesys Rondon                           ║");
+        System.out.println("║ Backend con JAVA 17                                    ║");
+        System.out.println("╠════════════════════════════════════════════════════════╣");
+        System.out.println("║ Usando API de: https://gutendex.com/?ref=public_apis   ║");
+        System.out.println("╚════════════════════════════════════════════════════════╝");
+   }
+
+   public void Despedida(){
+       System.out.println("Despedida");
+   }
+}
