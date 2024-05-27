3e0e623ab559ae77086023169523cbf400b76f24
luys <sanchezluys@gmail.com>
2024-05-26 19:42:32 -0500

    se crea el menu con las opciones 1,2,3 y salir como modelos. funcionando ok

--- screenmatch/src/main/java/com/aluracursos/sanchezluys/screenmatch/RetoLibros.java
+++ screenmatch/src/main/java/com/aluracursos/sanchezluys/screenmatch/RetoLibros.java
@@ -1,9 +1,10 @@
 package com.aluracursos.sanchezluys.screenmatch;
-
 import com.aluracursos.sanchezluys.screenmatch.principal.Menus;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class RetoLibros implements CommandLineRunner {
@@ -16,6 +17,8 @@ public class RetoLibros implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Menus menu = new Menus();
 		menu.Encabezado();
+		menu.Menu();
+		menu.RevisarSeleccion();
 
 	}
 }
